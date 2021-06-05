af06791b879176bc86ccc282acc99230cd524347
eru123 <yeoligoakino@gmail.com>
2021-06-05 15:49:07 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # choco-kanban
-Choco Kanban (pronounced as Koh-ko' Kan-ban) is an advanced scheduling system to manage complex workflow.
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  productionSourceMap: false,
+  configureWebpack: {
+    optimization: {
+      splitChunks: false,
+    },
+  },
+  css: {
+    extract: true,
+  },
+  chainWebpack: (config) => {
+    config.plugin("html").tap((args) => {
+      args[0].title = require("./project.json").appName;
+      return args;
+    });
+  },
+  pwa: {
+    name: require("./project.json").appName,
+    themeColor: "#f1f1f1",
+    msTileColor: "#f1f1f1",
+    assetsVersion: require("./package.json").version,
+  },
+  devServer: {
+    public: "localhost:8080",
+    disableHostCheck: true,
+  },
+};
